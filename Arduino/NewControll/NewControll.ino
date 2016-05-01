9b6cb87bcaf2c85557bf0aa3f112787bb102efb5
daniloqb <daniloqb@gmail.com>
2016-05-01 01:18:30 -0300

    Funções de Farol e Buzina adicionadas

--- Arduino/NewControll/NewControll.ino
+++ Arduino/NewControll/NewControll.ino
@@ -4,12 +4,22 @@
 
 
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial mySerial(7, 8); // RX, TX
 
-int IN1 = 4;
+int IN1 = 3;
 int IN2 = 5;
 int IN3 = 6;
-int IN4 = 7;
+int IN4 = 9;
+
+int headlight = 11;
+int backlight = 12;
+int buzzer = 10;
+
+byte lights = 0;
+byte lightsState = 0;
+byte buzzerOn = 0;
+byte buzzerState = 0;
+
 byte command;
 byte front, back, left, right;
 void setup()
@@ -18,11 +28,21 @@ void setup()
  pinMode(IN2, OUTPUT);
  pinMode(IN3, OUTPUT);
  pinMode(IN4, OUTPUT);
- 
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,LOW);
-    digitalWrite(IN3,LOW);
-    digitalWrite(IN4,LOW);
+
+ pinMode(headlight, OUTPUT);
+ pinMode(backlight, OUTPUT);
+ pinMode(buzzer,OUTPUT);
+
+ digitalWrite(IN1,LOW);
+ digitalWrite(IN2,LOW);
+ digitalWrite(IN3,LOW);
+ digitalWrite(IN4,LOW);
+
+ digitalWrite(headlight,LOW);
+ digitalWrite(backlight,LOW);
+ noTone(buzzer);
+
+
     
   mySerial.begin(9600);
 }
@@ -40,11 +60,33 @@ void loop() // run over and over
     back = bool(command & 2);
     left = bool(command & 4);
     right = bool(command & 8);
+
+    lights =  bool(command & 16);
+    buzzerOn = bool(command & 32);
+    
     digitalWrite(IN3,left);
     digitalWrite(IN4,right); 
     digitalWrite(IN1,front);
     digitalWrite(IN2,back);
-    
+
+    if(lights){
+      lightsState = (lightsState + 85)%340; 
+      analogWrite(headlight,lightsState);
+      digitalWrite(backlight,lightsState);      
+      }
+
+    if(buzzerOn){
+
+      buzzerState = !buzzerState;
+
+      if(buzzerState){
+        tone(buzzer,440);
+      }
+      else{
+        noTone(buzzer);
+      }
+      
+    }
 
     
     
