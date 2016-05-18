0ec1f34b69a33628b8806cfe62a0f1a137926232
olivato <olivatooo@gmail.com>
2016-05-17 21:58:09 -0300

    App na PlayStore

--- Android/BluetoothController/app/src/main/java/bluecontrol/game/olivato/bluetoothcontroller/MainMenu.java
+++ Android/BluetoothController/app/src/main/java/bluecontrol/game/olivato/bluetoothcontroller/MainMenu.java
@@ -1,8 +1,13 @@
 package bluecontrol.game.olivato.bluetoothcontroller;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 /**
@@ -15,6 +20,46 @@ public class MainMenu extends Activity{
         setContentView(R.layout.main_menu);
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()) {
+            case R.id.sobre:
+            {
+                new AlertDialog.Builder(this)
+                        .setTitle("Criado por Eletronic Drops")
+                        .setMessage("Criado por programadores , para programadores. Caso você não saiba usar, não seja ignorante" +
+                                " acesse https://github.com/electronicdrops/RcCarController e aprenda como funciona! Criadores:\n *Lucas Baquião" +
+                                "\n *Danilo Queiroz Barbosa \n *Gabriel Olivato")
+                        .setPositiveButton("Acessar", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                String url = "https://github.com/electronicdrops/RcCarController";
+                                Intent i = new Intent(Intent.ACTION_VIEW);
+                                i.setData(Uri.parse(url));
+                                startActivity(i);
+                            }
+                        })
+                        .setNegativeButton("Sair", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+                break;
+            }
+        }
+        return true;
+    }
+
+
     public void GotoBasicsOne(View view)
     {
         Intent i = new Intent(getApplicationContext(), MainActivity.class);
@@ -25,5 +70,11 @@ public class MainMenu extends Activity{
         Intent i = new Intent(getApplicationContext(), BasicsTwo.class);
         startActivity(i);
     }
-
+    public void vaiprogit(View view)
+    {
+        String url = "https://github.com/electronicdrops/RcCarController";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
 }
